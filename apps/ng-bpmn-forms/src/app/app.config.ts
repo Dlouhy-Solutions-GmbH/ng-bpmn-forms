6125fd7a946fb3a367abebb064971ffcb01750c3
David Dlouhy <david@dlouhy.io>
2025-07-19 15:02:38 +0200

    remove unused validation message functions from app.config.ts

--- apps/ng-bpmn-forms/src/app/app.config.ts
+++ apps/ng-bpmn-forms/src/app/app.config.ts
@@ -9,50 +9,6 @@ import Aura from '@primeuix/themes/aura';
 import {provideHttpClient} from "@angular/common/http";
 import {FormlyFieldTextArea} from "./textarea.type";
 
-// export function minItemsValidationMessage(error: any, field: FormlyFieldConfig) {
-//   return `should NOT have fewer than ${field.props.minItems} items`;
-// }
-//
-// export function maxItemsValidationMessage(error: any, field: FormlyFieldConfig) {
-//   return `should NOT have more than ${field.props.maxItems} items`;
-// }
-//
-// export function minLengthValidationMessage(error: any, field: FormlyFieldConfig) {
-//   return `should NOT be shorter than ${field.props.minLength} characters`;
-// }
-//
-// export function maxLengthValidationMessage(error: any, field: FormlyFieldConfig) {
-//   return `should NOT be longer than ${field.props.maxLength} characters`;
-// }
-//
-// export function minValidationMessage(error: any, field: FormlyFieldConfig) {
-//   return `should be >= ${field.props.min}`;
-// }
-//
-// export function maxValidationMessage(error: any, field: FormlyFieldConfig) {
-//   return `should be <= ${field.props.max}`;
-// }
-//
-// export function multipleOfValidationMessage(error: any, field: FormlyFieldConfig) {
-//   return `should be multiple of ${field.props.step}`;
-// }
-//
-// export function exclusiveMinimumValidationMessage(error: any, field: FormlyFieldConfig) {
-//   return `should be > ${field.props.exclusiveMinimum}`;
-// }
-//
-// export function exclusiveMaximumValidationMessage(error: any, field: FormlyFieldConfig) {
-//   return `should be < ${field.props.exclusiveMaximum}`;
-// }
-//
-// export function constValidationMessage(error: any, field: FormlyFieldConfig) {
-//   return `should be equal to constant "${field.props.const}"`;
-// }
-//
-// export function typeValidationMessage({ schemaType }: any) {
-//   return `should be "${schemaType[0]}".`;
-// }
-
 export const appConfig: ApplicationConfig = {
   providers: [
     provideAnimationsAsync(),
