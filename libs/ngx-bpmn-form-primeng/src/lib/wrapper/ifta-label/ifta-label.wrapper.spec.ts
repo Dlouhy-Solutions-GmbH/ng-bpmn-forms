6bd7cf491465fdf3d471a33c78d6aa867c569372
David Dlouhy <david@dlouhy.io>
2025-08-01 14:05:21 +0200

    refactor(forms): rewrite `ifta-label` wrapper tests and remove unused Tailwind config
    Enhanced test coverage for `ifta-label` wrapper using `createFieldComponent` and `provideFormlyCore`. Removed the obsolete Tailwind configuration for `ngx-bpmn-form-primeng`.

--- libs/ngx-bpmn-form-primeng/src/lib/wrapper/ifta-label/ifta-label.wrapper.spec.ts
+++ libs/ngx-bpmn-form-primeng/src/lib/wrapper/ifta-label/ifta-label.wrapper.spec.ts
@@ -1,22 +1,66 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormlyFieldConfig, provideFormlyCore } from '@ngx-formly/core';
+import { createFieldComponent } from '@ngx-formly/core/testing';
+import { withPrimeNG } from './../../primeng.config';
 
-import { IftaLabelWrapper } from './ifta-label.wrapper';
+const renderComponent = (field: FormlyFieldConfig) => {
+  return createFieldComponent(field, {
+    providers: [provideFormlyCore([...withPrimeNG()])],
+  });
+};
 
-describe('IftaLabelWrapper', () => {
-  let component: IftaLabelWrapper;
-  let fixture: ComponentFixture<IftaLabelWrapper>;
+describe('ui-primeng: ifta-label Wrapper', () => {
+  xit('should render ifta-label wrapper', () => {
+    const { query } = renderComponent({
+      wrappers: ['ifta-label'],
+      props: {
+        label: 'Name',
+        required: true,
+        description: 'Name description',
+      },
+    });
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [IftaLabelWrapper],
-    }).compileComponents();
+    expct(query'formly-wrapper-primeng-ifta-label')).not.toBeNull();
 
-    fixture = TestBed.createComponent(IftaLabelWrapper);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    // Label + Required marker
+    expect(query('label').nativeElement.textContent).toEqual(' Name *');
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  xit('should show error message', () => {
+    const { query } = renderComponent({
+      key: 'name',
+      wrappers: ['ifta-label'],
+      validation: { show: true },
+      props: {
+        label: 'Name',
+        required: true
+      }
+    });
+
+    expect(query('formly-validation-message')).not.toBeNull();
+  });
+
+  it('should hide required marker', () => {
+    const { query } = renderComponent({
+      wrappers: ['ifta-label'],
+      props: {
+        label: 'Name',
+        hideRequiredMarker: true,
+        required: true
+      }
+    });
+
+    expect(query('label').nativeElement.textContent).toEqual(' Name ');
+  });
+
+  it('should hide label', () => {
+    const { query } = renderComponent({
+      wrappers: ['ifta-label'],
+      props: {
+        label: 'Name',
+        hideLabel: true
+      }
+    });
+
+    expect(query('label')).toBeNull();
   });
 });
